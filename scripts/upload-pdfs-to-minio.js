d99bdd58e183a1325be87892697e3c4abeddbadb
Ali <ali.kalate89@hotmail.com>
2025-10-30 21:12:40 +0330

    fix the upload to minio script

--- scripts/upload-pdfs-to-minio.js
+++ scripts/upload-pdfs-to-minio.js
@@ -13,7 +13,7 @@ const {
   MINIO_ACCESS_KEY = 'minioadmin',
   MINIO_SECRET_KEY = 'minioadmin',
   BUCKET = 'assistant-aggregator',
-  FILE_DIR = './General_Law_PDFs',
+  FILE_DIR = '../files/General_Law_PDFs',
   MONGO_URI = 'mongodb://admin:password123@127.0.0.1:27017/assistant_aggregator?authSource=admin',
   SKIP_IF_CHECKSUM_EXISTS = 'true',
 } = process.env;
